cc0601fe8328d7696fcb84860328c65414ccc006
Serge Ordanyan <ordanyanserge@aol.com>
2025-05-28 21:57:16 +0400

    createing theme

---
+++ src/hoc/ChangeTheme.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ChangeTheme(props) {
+  return (
+    <div>
+<p>Change Theme Provider</p>
+    </div>
+  )
+}
+
+.propTypes = {}
+
+export default ChangeTheme
+
