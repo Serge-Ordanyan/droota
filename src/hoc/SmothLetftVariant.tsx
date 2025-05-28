2cce988ab7a6d9b6b006282a9192aae88d722904
Serge Ordanyan <ordanyanserge@aol.com>
2025-05-28 21:58:50 +0400

    createing Smooth Left

---
+++ src/hoc/SmothLetftVariant.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SmothleftVariant = () => {
+  return (
+    <View>
+      <Text>Smooth Left Varian</Text>
+    </View>
+  );
+};
+
+export default SmothleftVariant;
+
+const styles = StyleSheet.create({});
