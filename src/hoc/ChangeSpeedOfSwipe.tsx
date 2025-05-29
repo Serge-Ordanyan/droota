1af98031ee8815eeadea49648f2fd47e7230899d
Serge Ordanyan <ordanyanserge@aol.com>
2025-05-29 20:56:15 +0400

    createing Change Speed of Swipe

---
+++ src/hoc/ChangeSpeedOfSwipe.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ChangeSpeedOfSwipe = () => {
+  return (
+    <View>
+      <Text>ChangeSpeedOfSwipe</Text>
+    </View>
+  );
+};
+
+export default ChangeSpeedOfSwipe;
+
+const styles = StyleSheet.create({});
